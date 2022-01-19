bb90171c3d7791e48c317104ae9f32b05b705ff6
Fatih Canbey <fcanbeyy@gmail.com>
2022-01-19 03:52:22 +0300

    typo at restaurant-info-card on import text

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -4,7 +4,7 @@ import { SvgXml } from "react-native-svg";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
 import { Spacer } from "../../../components/spacer/spacer.component";
-import { Text } from "../../../components/typograghy/text.component";
+import { Text } from "../../../components/typography/text.component";
 
 import {
   RestaurantCard,
