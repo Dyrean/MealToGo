e250aedfd47bfd1e6c29e37134b8472ca4409966
Fatih Canbey <fcanbeyy@gmail.com>
2022-01-21 14:43:00 +0300

    feature: Map View added to map screen and rendering the location of restaurants by searched location

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,11 +1,51 @@
-import React from "react";
-import { Text } from "react-native";
-import { SafeArea } from "../../../components/utility/safe-area.component";
+import React, { useState, useContext, useEffect } from "react";
+import MapView from "react-native-maps";
+import styled from "styled-components/native";
+
+import { LocationContext } from "../../../services/location/location.context";
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import { Search } from "../components/search.component";
+
+const Map = styled(MapView)`
+  height: 100%;
+  width: 100%;
+`;
 
 export const MapScreen = () => {
+  const { location } = useContext(LocationContext);
+  const { restaurants } = useContext(RestaurantsContext);
+  const [latDelta, setLatDelta] = useState(0);
+  const { lat, lng, viewport } = location;
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
   return (
-    <SafeArea>
-      <Text>Map Screen</Text>
-    </SafeArea>
+    <>
+      <Search />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}
+      >
+        {restaurants.map((restaurant) => {
+          return (
+            <MapView.Marker
+              key={restaurant.name}
+              title={restaurant.name}
+              coordinate={{
+                latitude: restaurant.geometry.location.lat,
+                longitude: restaurant.geometry.location.lng,
+              }}
+            />
+          );
+        })}
+      </Map>
+    </>
   );
 };
