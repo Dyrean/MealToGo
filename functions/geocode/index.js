2ab04a69423edfa4741e53606978b7cd5a7f937d
Fatih Canbey <fcanbeyy@gmail.com>
2022-01-24 19:41:46 +0300

    refactor: mocks relocated to firebase functions and firebase added

---
+++ functions/geocode/index.js
@@ -0,0 +1,8 @@
+const { locations: locationsMock } = require("./geocode.mock");
+const url = require("url");
+
+module.exports.geocodeRequest = (request, response) => {
+  const { city } = url.parse(request.url, true).query;
+  const locationMock = locationsMock[city.toLocaleLowerCase()];
+  response.json(locationMock);
+};
--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,14 +1,10 @@
 import camelize from "camelize";
 
-import { locations } from "./location.mock";
-
 export const locationRequest = (searchTerm) => {
-  return new Promise((resolve, reject) => {
-    const locationMock = locations[searchTerm];
-    if (!locationMock) {
-      reject("not found");
-    }
-    resolve(locationMock);
+  return fetch(
+    `http://localhost:5001/mealstogo-97791/us-central1/geocode?city=${searchTerm}`
+  ).then((res) => {
+    return res.json();
   });
 };
 
